4c141dd92a5c81e1aed74f45af68060ff0002d25
Julinhainformaticas <Barbosa.julia@aluno.ifsp.edu.br>
2024-02-21 11:15:36 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1;
+            int n2;
+            int soma;
+            int sub;
+            int mult;
+            int div;
+
+
+            Console.WriteLine("escolha um número");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("escolha outro número, divisivel pelo anterior");
+            n2 = int.Parse(Console.ReadLine());
+            soma = n1 + n2;
+            Console.WriteLine("soma = {0}", soma );
+            sub = n1 - n2;
+            Console.WriteLine("subtração = {0}", sub);
+            div = n1 / n2;
+            Console.WriteLine("divisão = {0}", div);
+            mult = n1 * n2;
+            Console.WriteLine("multiplicação = {0}", mult);
+
+        }
+    }
+}
+
